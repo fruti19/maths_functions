5431641aaac134ac9e7f42e3ac69c884d73755c6
Dhruti Patel <P13234946@KLIXX201LIN007W.lec-admin.dmu.ac.uk>
2016-03-08 18:19:51 +0000

    maths

---
+++ maths.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 12;
+    int y = 8;
+    printf ("%i + %i = %i\n", x, y, x+y);
+    printf ("%i - %i = %i\n", x, y, x-y);
+    printf ("%i * %i = %i\n", x, y, x*y);
+    printf ("%i / %i = %i\n", x, y, x/y);
+    return 0;
+}
